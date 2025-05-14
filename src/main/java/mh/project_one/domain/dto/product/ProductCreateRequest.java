e76ec63b09a054b66225e52d95d860b498aacee5
구명회 <sl>
2025-05-14 17:18:20 +0900

    일단 dto 개념공부중...

---
+++ src/main/java/mh/project_one/domain/dto/product/ProductCreateRequest.java
@@ -0,0 +1,32 @@
+package mh.project_one.domain.dto.product;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ProductCreateRequest {
+
+    private String productName;
+
+    private String productDescription;
+
+    private BigDecimal productPrice;
+
+    private Integer stockQuantity;
+
+    private Boolean isAvailable;
+
+    private LocalDate releaseDate;
+
+    private String categoryCode;
+
+    private String manufacturerInfo;
+
+}
---
+++ src/main/java/mh/project_one/domain/dto/product/ProductSummaryResponse.java
@@ -0,0 +1,14 @@
+package mh.project_one.domain.dto.product;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ProductSummaryResponse {
+
+}
